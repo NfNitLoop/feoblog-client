c8edb3909df7a1e0c663d75c3dfd43a9dd3c98b5
Cody Casterline <cody.casterline@gmail.com>
2021-07-03 20:05:44 -0700

    Use fixed base58check. (Thanks!)

--- private/deps.ts
+++ private/deps.ts
@@ -1,10 +1,12 @@
+
+// UserIDs, Signatures, and PrivateKeys are represented in base58(check):
+export * as b58c from "https://deno.land/x/base58check@v0.1.4/mod.ts"
+
+// UserIDs can also be displayed as hex:
 export * as hex from "https://deno.land/std@0.100.0/encoding/hex.ts"
+
+// Used for signing/verifiying Item metadata.
 export { default as nacl } from "https://deno.land/x/sodium@0.2.0/basic.ts"
 
-// export * as b58c from "../../noble-base58check/mod.ts"
-// TODO: Use a deno.land version once this issues is fixed:
-//       https://github.com/serh11p/noble-base58check/issues/1
-export * as b58c from "https://www.nfnitloop.com/deno/noble-base58check/mod.ts"
-
-
-// See also: ./private/protobuf/google_protobufs.ts, which I included just to make the import look nicer in feoblog.ts. :p
+// See also: ./private/protobuf/google_protobufs.ts
+// ... which I included just to make the import look nicer in feoblog.ts. :p
